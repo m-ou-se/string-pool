b6f374eedbe946399d29dae98a0453a4f6b97054
Maurice Bos <m-ou.se@m-ou.se>
2015-12-28 16:46:23 +0100

    Update example.

--- example/example.cpp
+++ example/example.cpp
@@ -5,28 +5,28 @@
 int main() {
 	string_tracker p;
 
-	auto f1 = p.add_file("../string_tracker.hpp");
-	auto f2 = p.add_file("../string_pool.hpp");
+	auto f1 = p.add_file("example-input-1");
+	auto f2 = p.add_file("example-input-2");
 
 	if (!f1 || !f2) {
 		std::cerr << "Unable to open files." << std::endl;
 		return 1;
 	}
 
-	auto s1 = f1->substr(19, 10);
-	auto s2 = f1->substr(54, 7);
-	auto s3 = f2->substr(98, 20);
+	auto s1 = f1->substr(6, 5);
+	auto s2 = f1->substr(23, 4);
+	auto s3 = f2->substr(28, 3);
 	std::cout << p.location(s1) << ": " << s1 << std::endl;
 	std::cout << p.location(s2) << ": " << s2 << std::endl;
 	std::cout << p.location(s3) << ": " << s3 << std::endl;
 
 	auto b = p.builder();
-	b.append(f1->substr(8, 4));
-	b.append("foobar");
-	b.append(f2->substr(15, 7));
+	b.append(f1->substr(13, 9));
+	b.append("n example: ");
+	b.append(f2->substr(24, 11));
 	auto s4 = b.build();
-	auto s5 = s4.substr(7,3);
-	auto s6 = s4.substr(10);
+	auto s5 = s4.substr(11,7);
+	auto s6 = s4.substr(20);
 	std::cout << p.location(s4) << ": " << s4 << std::endl;
 	std::cout << p.location(s5) << ": " << s5 << std::endl;
 	std::cout << p.location(s6) << ": " << s6 << std::endl;
