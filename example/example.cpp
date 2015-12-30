6028edde74fe4814953bd57f9290ad3573c0dfa0
Maurice Bos <m-ou.se@m-ou.se>
2015-12-30 13:41:17 +0100

    Put types instead of auto in example for clarity.

--- example/example.cpp
+++ example/example.cpp
@@ -1,21 +1,23 @@
 #include <iostream>
 
+#include "optional.hpp"
 #include "string_tracker.hpp"
+#include "string_view.hpp"
 
 int main() {
 	string_tracker p;
 
-	auto f1 = p.add_file("example-input-1");
-	auto f2 = p.add_file("example-input-2");
+	optional<string_view> f1 = p.add_file("example-input-1");
+	optional<string_view> f2 = p.add_file("example-input-2");
 
 	if (!f1 || !f2) {
 		std::cerr << "Unable to open files." << std::endl;
 		return 1;
 	}
 
-	auto s1 = f1->substr(6, 5);
-	auto s2 = f1->substr(23, 4);
-	auto s3 = f2->substr(28, 3);
+	string_view s1 = f1->substr(6, 5);
+	string_view s2 = f1->substr(23, 4);
+	string_view s3 = f2->substr(28, 3);
 	std::cout << p.location(s1) << ": " << s1 << std::endl;
 	std::cout << p.location(s2) << ": " << s2 << std::endl;
 	std::cout << p.location(s3) << ": " << s3 << std::endl;
@@ -24,9 +26,9 @@ int main() {
 	b.append(f1->substr(13, 9));
 	b.append("n example: ");
 	b.append(f2->substr(24, 11));
-	auto s4 = b.build();
-	auto s5 = s4.substr(11,7);
-	auto s6 = s4.substr(20);
+	string_view s4 = b.build();
+	string_view s5 = s4.substr(11,7);
+	string_view s6 = s4.substr(20);
 	std::cout << p.location(s4) << ": " << s4 << std::endl;
 	std::cout << p.location(s5) << ": " << s5 << std::endl;
 	std::cout << p.location(s6) << ": " << s6 << std::endl;
