341288a75e4e4cae3191906ffd70d6dcc8432fc5
Maurice Bos <m-ou.se@m-ou.se>
2017-10-05 15:58:52 +0200

    Use std::optional when available.

--- include/optional.hpp
+++ include/optional.hpp
@@ -1,4 +1,9 @@
+#if (__cplusplus <= 201402L || !__has_include(<optional>)) && __has_include(<experimental/optional>)
 #include <experimental/optional>
-
 using std::experimental::optional;
 using std::experimental::nullopt;
+#else
+#include <optional>
+using std::optional;
+using std::nullopt;
+#endif
